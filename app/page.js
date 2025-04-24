7468201f74991989a8280a48910159287259c0bb
chathuwa-whiz <navodchathushka@gmail.com>
2025-04-24 19:00:39 +0530

    Add pixels section with creative depth image animation

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Header from './home/header.js'
 import Hero from './home/hero.js'
-// import Pixels from './home/pixels.jsx'
-// import Precision from './home/precision.jsx'
-// import Designs from './home/designs.jsx'
-// import Carousel from './home/carousel.jsx'
-// import Bgimage from './home/bgimage.jsx'
-// import Footer from './home/footer.jsx'
+import Pixels from './home/pixels.js'
+// import Precision from './home/precision.js'
+// import Designs from './home/designs.js'
+// import Carousel from './home/carousel.js'
+// import Bgimage from './home/bgimage.js'
+// import Footer from './home/footer.js'
 
 export default function Home() {
   return (
@@ -16,22 +16,22 @@ export default function Home() {
       <div className='bg-stone-300 w-full'>
         <Hero />
       </div>
-      {/* <div className='bg-black w-full'>
+      <div className='bg-black w-full'>
         <Pixels />
       </div>
-      <div className='bg-black w-full'>
+      {/* <div className='bg-black w-full'>
         <Precision />
-      </div>
-      <div className='bg-stone-300 w-full'>
+      </div> */}
+      {/* <div className='bg-stone-300 w-full'>
         <Designs />
-      </div>
-      <div className='bg-black w-full'>
+      </div> */}
+      {/* <div className='bg-black w-full'>
         <Carousel />
-      </div>
-      <div style={{ backgroundImage: `url(/images/launching.png)` }} className='bg-cover bg-center w-full h-screen'>
+      </div> */}
+      {/* <div style={{ backgroundImage: `url(/images/launching.png)` }} className='bg-cover bg-center w-full h-screen'>
         <Bgimage />
-      </div>
-      <div className='bg-stone-300 w-full'>
+      </div> */}
+      {/* <div className='bg-stone-300 w-full'>
         <Footer />
       </div> */}
     </div>
